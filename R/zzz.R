5403f2f84473488d499121ba3366282ef4b6370f
dracodoc <dracodoc@gmail.com>
2017-01-16 22:09:13 -0500

    on package init message

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,11 @@
 .onAttach <- function(libname, pkgname) {
-  # packageStartupMessage(ifelse(found_carto_env(),
-  #                              "Carto user name",
-  #                              # paste0("Carto user name ",
-  #                              #        carto_env["carto_acc"],
-  #                              #        " and API key found in environment"),
-  #                              "Carto user name and API key not found in environment or invalid, check `help carto_setup` for details"
-  #                              ))
+  carto_env <- carto_setup()
+  packageStartupMessage(ifelse(typeof(carto_env) == "character",
+                               # "Carto user name",
+                               paste0("Carto user name ",
+                                      carto_env["carto_acc"],
+                                      " and API key found in environment"),
+                               "Carto user name and API key not found in environment or invalid, check `help carto_setup` for details"
+                               ))
+  # packageStartupMessage("test")
 }
